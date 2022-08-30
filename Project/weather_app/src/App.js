c4aec2aa37db4bfd7168b40594d0f8dba8ea314e
Rola <cloudgit.241c2@simplelogin.co>
2022-08-30 17:16:01 -0500

    fetched api

--- Project/weather_app/src/App.js
+++ Project/weather_app/src/App.js
@@ -1,17 +1,43 @@
 import './App.css';
 import { Heading } from './component/Heading/Heading';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [weather, setWeather] = useState();
+
+  const apiKey = '737d76764aadcf0f2bb5a71eb378395c';
+  const cityName = 'Austin';
+  const stateName = 'Texas';
+  const countryCode = 'US';
+  
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},${countryCode}&appid=${apiKey}`);
+      const data = response.json();
+      setWeather(data)
+    }
+    fetchWeather();
+  }, []);
+
+
+
+
+  
+  
+  
   return (
     <div className="App">
      
      <header className='header'>
         <Heading />
      </header>
+     <nav>
+      <p>Search Bar goes here</p>
+     </nav>
+     <main>
 
-      <nav>
+     </main>
 
-      </nav>
     </div>
   );
 }
