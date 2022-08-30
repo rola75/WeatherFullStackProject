49e1d54d00cd1cbf12b632eba585263dd23f6718
Rola <cloudgit.241c2@simplelogin.co>
2022-08-30 15:20:16 -0500

    added the header component

---
+++ Project/weather_app/src/component/Heading/Heading.jsx
@@ -0,0 +1,10 @@
+import "./Heading.scss"
+
+export const Heading = () => {
+
+    return (
+        <header className="app_heading">
+            <h1 className="app_heading_title">Weather App</h1>
+        </header>
+    )
+}
